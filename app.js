3340f1575393be82aa098630604150116ff3b8a4
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-02-11 06:22:28 -0300

    inicia projeto e cria primeiros passos

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jodo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 a 10');
+
+if (chute == numeroSecreto) {
+    console.log('SUCESSO! Voce descobriu o numero secreto (5)');
+}
