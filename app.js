cda704731fcd12d9a350cc5c2147320baee6f467
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-02-12 05:07:35 -0300

    Implementa loop e numero de tentativas

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-alert('Boas vindas ao jodo do número secreto');
+//alert('Boas vindas ao jodo do número secreto');
 let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 a 10');
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-    console.log('SUCESSO! Voce descobriu o numero secreto (5)');
+var acertou = 'PARABÉNS! Voçê acertou o número secreto'
+var errou = 'Não foi dessa vez, você errou :(';
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 a 10');
+    if (chute == numeroSecreto) {
+        // poedia ser assim = alert(`PARABÉNS! Voçê acertou o número secreto ${numeroSecreto}`);
+        alert(acertou + ' ' + numeroSecreto + ' com ' + tentativas + ' chutes');
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor - ${chute}`);
+        } else {
+            alert(`O número secreto é maior - ${chute}`);
+        }
+        //tentativas = tentativas + 1
+        tentativas++
+    }
 }
