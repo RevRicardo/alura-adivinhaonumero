9185262bef7c63c1eeb09483b9b5c82362958586
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-02-12 06:04:17 -0300

    implementou inserção numeroMaximo e gerar numero aleatorio

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //alert('Boas vindas ao jodo do número secreto');
-let numeroSecreto = 5;
+let numeroMaximo;
+numeroMaximo = prompt(`Digite o número entre 100 a 1000`);
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
@@ -7,10 +10,9 @@ var acertou = 'PARABÉNS! Voçê acertou o número secreto'
 var errou = 'Não foi dessa vez, você errou :(';
 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 a 10');
+    chute = prompt(`Escolha um número entre 1 a ${numeroMaximo}`);
     if (chute == numeroSecreto) {
-        // poedia ser assim = alert(`PARABÉNS! Voçê acertou o número secreto ${numeroSecreto}`);
-        alert(acertou + ' ' + numeroSecreto + ' com ' + tentativas + ' chutes');
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor - ${chute}`);
@@ -21,3 +23,16 @@ while (chute != numeroSecreto) {
         tentativas++
     }
 }
+
+//Operador ternário
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`${acertou} ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+//Todo if / else abaixo foi alterado para as linhas de cima
+//if (tentativas > 1) {
+    // poedia ser assim = alert(`Voçê acertou ${numeroSecreto}`);
+    //alert(acertou + ' ' + numeroSecreto + ' com ' + tentativas + ' chutes');
+//    alert(`${acertou} ${numeroSecreto} com ${tentativas} chutes`);
+//} else {
+//    alert(`${acertou} ${numeroSecreto} com ${tentativas} chute`);
+//}
